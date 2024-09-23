de2a7ae3b03856203cd47eb2071eef08fa9a265a
mayorr-star <immaboat11@gmail.com>
2024-09-23 19:31:18 +0000

    Require getCurrentYear function
    To render it as an argument to index view

--- db/queries.js
+++ db/queries.js
@@ -1,12 +1,46 @@
-const pool = require("pool");
+const pool = require("./pool");
 
 const fecthAllProduce = async () => {
   const { rows } = await pool.query(
-    "SELECT produce.common_name, produce.scientific_name, produce.count, produce.unit, categories.category, land_area.area FROM NanaFarms INNER JOIN produce_category ON produce.id = produce_category.produce_id INNER JOIN categories ON produce_category.category_id = category.id INNER JOIN produce_land ON produce_land.produce_id = produce.id INNER JOIN land_area ON produce_land.land_id = land_area.id"
+    "SELECT produce.common_name, produce.scientific_name, produce.count, produce.unit, categories.category, landarea.area_acres FROM produce INNER JOIN produce_category ON produce.id = produce_category.produce_id INNER JOIN categories ON produce_category.category_id = categories.id INNER JOIN produce_land ON produce_land.produce_id = produce.id INNER JOIN landarea ON produce_land.land_id = landarea.id"
   );
   return rows;
 };
+const getCategories = async () => {
+  const { rows } = await pool.query("SELECT category FROM categories");
+  return rows;
+}
 
-const insertNewProduce = async () => {
-  await pool.query("INSERT INTO produce (common_name scientific_name, count, unit) VALUES ($1, $2, $3, $4) ");
+const insertProduceItem = async (
+  common_name,
+  scientific_name,
+  count,
+  unit,
+  price
+) => {
+  await pool.query(
+    "INSERT INTO produce (common_name scientific_name, count, unit) VALUES ($1, $2, $3, $4, $5)",
+    [common_name, scientific_name, count, unit, price]
+  );
+};
+
+const insertLandArea = async (landArea) => {
+  await pool.query("INSERT INTO land (area) VALUES ($1)", [landArea]);
+};
+
+const deleteProduceItem = async (produceId) => {
+  await pool.query("SELECT * FROM produce WHERE id = $1", [produceId]);
+};
+
+const updateProduceItem = async (produceId) => {
+  await pool.query("SELECT * produce  WHERE id = $1", [produceId]);
+};
+
+module.exports = {
+  fecthAllProduce,
+  insertLandArea,
+  insertProduceItem,
+  deleteProduceItem,
+  updateProduceItem,
+  getCategories
 };
