74fd7c18503bf158b8edebce6b57c3068d00c1e4
mayorr-star <immaboat11@gmail.com>
2024-09-23 19:28:02 +0000

    Render view to show all produce

--- controllers/produceController.js
+++ controllers/produceController.js
@@ -1,24 +1,49 @@
 const asyncHandler = require('express-async-handler');
+const db = require('../db/queries');
+const getCurrentYear = require('../utilis/year');
+const currentYear = getCurrentYear();
 
 const getAllProduce = asyncHandler(async(req, res)=> {
-    console.log('all produce');
+    const farmProduce = await db.fecthAllProduce();
+    const categories = await db.getCategories();
+    res.render('index', {categories: categories, produce: farmProduce, year: currentYear});
 })
 
 const createNewProduce = asyncHandler(async(req, res)=> {
+    const { common_name, scientific_name, count, unit, price} = req.body;
+    await db.insertProduceItem(common_name, scientific_name, count, unit, price);
+    await db.insertLandArea(area);
+    res.redirect('/');
     console.log('produce created');
 })
 
 const deleteProduce = asyncHandler(async(req, res)=> {
+    const { produceId } = req.params;
+    await db.deleteProduceItem(produceId);
+    res.redirect('/');
     console.log('produce deleted');
 })
 
 const updateProduce = asyncHandler(async(req, res)=> {
+    const {produceId} = req.params;
+    await db.updateProduceItem(produceId)
+    res.redirect('/');
     console.log('produce updated');
+})
+
+const getNewItemForm = asyncHandler(async(req, res) => {
+    res.render('form', {produce});
+})
+
+const getProduce = asyncHandler(async(req, res) => {
+    res.send('form');
 })
 
 module.exports = {
     getAllProduce,
     createNewProduce,
     deleteProduce,
-    updateProduce
+    updateProduce,
+    getNewItemForm,
+    getProduce
 }
