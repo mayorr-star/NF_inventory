db9b88432fd50dc71428ead6dcdeb81c4130e658
mayorr-star <immaboat11@gmail.com>
2024-09-23 20:56:27 +0000

    Add add-new-produce route.
    To display form to add details of new item.

--- routes/produceRouter.js
+++ routes/produceRouter.js
@@ -3,6 +3,8 @@ const produceController = require("../controllers/produceController");
 const router = Router();
 
 router.get("/", produceController.getAllProduce);
+router.get("/add-new-produce", produceController.getNewItemForm);
+router.get("/search/:produceId", produceController.getProduce);
 router.post("/add-new-produce", produceController.createNewProduce);
 router.get("/delete/produce/:produceId", produceController.deleteProduce);
 router.get("/update/produce/:produceId", produceController.updateProduce);
