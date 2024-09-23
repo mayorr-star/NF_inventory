b927c1f07f532d83ad9d4ed96a84962ecda3efee
mayorr-star <immaboat11@gmail.com>
2024-09-23 20:58:50 +0000

    import notfounderrorhandler and use it in app.

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const path = require('path');
 const produceRouter = require("./routes/produceRouter");
-const app = express();
+const handleNotFoundError = require("./utilis/errorhandling/middlewares/notFoundError");
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.use("/", produceRouter);
+
+app.use(handleNotFoundError);
+
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
