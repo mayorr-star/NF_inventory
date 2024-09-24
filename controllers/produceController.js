50345ecce8ff0d7f9d16d12d06c6cbbc7bf67489
mayorr-star <immaboat11@gmail.com>
2024-09-24 20:21:51 +0000

    Require NotFoundErrorClass
    Handle not found errors.

--- controllers/produceController.js
+++ controllers/produceController.js
@@ -1,25 +1,32 @@
 const asyncHandler = require('express-async-handler');
 const db = require('../db/queries');
 const getCurrentYear = require('../utilis/year');
+const NotFoundErrorClass = require('../utilis/errorhandling/errors/NotFoundError');
+
 const currentYear = getCurrentYear();
 
 const getAllProduce = asyncHandler(async(req, res)=> {
-    const farmProduce = await db.fecthAllProduce();
+    const farmProduce = await db.getAllProduce();
     const categories = await db.getCategories();
     
-    if(!farmProduce || !categories) {
-        throw new NotFoundError("Not found");
+    if(!farmProduce) {
+        throw new NotFoundErrorClass("Farm produce not found!");
+    } else if (!categories) {
+        throw new NotFoundErrorClass('Categories not found!');
     }
+
     res.render('index', {categories: categories, produce: farmProduce, year: currentYear});
 })
 
 const createNewProduce = asyncHandler(async(req, res)=> {
-    const { cmName, sciName, numItems, unit, price, area} = req.body;
-    console.log(cmName, sciName, numItems, unit, price, area);
+    const { cmName, sciName, numItems, unit, price, land_size, category} = req.body;
     await db.insertProduceItem(cmName, sciName, numItems, unit, price);
-    await db.insertLandArea(area);
+    const produceId = await db.getProduceId(cmName);
+    const landId = await db.getLandId(land_size);
+    const categoryId = await db.getCategoryId(category);
+    await db.insertProduceIdLandId(produceId[0].id, landId[0].id);
+    await db.insertProduceIdCategoryId(produceId[0].id, categoryId[0].id)
     res.redirect('/');
-    console.log('produce created');
 })
 
 const deleteProduce = asyncHandler(async(req, res)=> {
@@ -38,7 +45,13 @@ const updateProduce = asyncHandler(async(req, res)=> {
 
 const getNewItemForm = asyncHandler(async(req, res) => {
     const categories = await db.getCategories();
-    res.render('produceForm', {year: currentYear, categories: categories});
+    const landSizes = await db.getLandSizes();
+    if (!categories) {
+        throw new NotFoundErrorClass('Form not found!');
+    } else if (!landSizes) {
+        throw new NotFoundErrorClass('Land sizes not found!');
+    }
+    res.render('produceForm', {year: currentYear, categories: categories, landSizes: landSizes});
 })
 
 const getProduce = asyncHandler(async(req, res) => {
