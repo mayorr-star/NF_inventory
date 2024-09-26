62b6504298b4ebcff76233a906b6795d3572223d
mayorr-star <immaboat11@gmail.com>
2024-09-26 09:57:08 +0000

    Send a json response

--- utilis/errorhandling/middlewares/notFoundError.js
+++ utilis/errorhandling/middlewares/notFoundError.js
@@ -1,10 +1,11 @@
 const handleNotFoundError = (err, req, res, next) => {
-    if (err.name === "NotFoundError") {
-      console.log(err.statusCode);
-      res.status(err.statusCode).send(err.message);
-    } else {
-      next();
-    }
+  if (err.name === "NotFoundError") {
+    console.log(`NotFoundError: ${err.statusCode} ${err.message}`);
+    res.status(err.statusCode).json({ error: err.message });
+  } else {
+    console.error(err.stack);
+    next(err);
+  }
 };
 
 module.exports = handleNotFoundError;
