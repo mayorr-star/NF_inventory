42436b2be964b556d1c1d436e528eccadf06909a
mayorr-star <immaboat11@gmail.com>
2024-09-28 12:41:48 +0000

    Add new route to get categoryForm

--- routes/categories.js
+++ routes/categories.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const categoriesController = require('../controllers/categoriesController');
 
+router.get('/form/create', categoriesController.getCategoryForm);
 router.get('/', categoriesController.getCategories);
 
 module.exports = router;
