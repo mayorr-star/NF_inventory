532404bd76fe053515d87425a3ceeec7983e0e15
mayorr-star <immaboat11@gmail.com>
2024-09-28 17:29:59 +0000

    Add post route to delete category

--- routes/categories.js
+++ routes/categories.js
@@ -3,7 +3,8 @@ const router = Router();
 const categoriesController = require('../controllers/categoriesController');
 
 router.get('/form/create', categoriesController.getCategoryForm);
-router.post("/create", categoriesController.createCategory)
+router.post("/create", categoriesController.createCategory);
+router.post("/delete/:categoryId", categoriesController.deleteCategory);
 router.get('/', categoriesController.getCategories);
 
 module.exports = router;
