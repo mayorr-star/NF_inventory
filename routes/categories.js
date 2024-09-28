33f05e52dba7b22e4500e719e0c7df0ec9880dba
mayorr-star <immaboat11@gmail.com>
2024-09-28 13:15:03 +0000

    Add post route to post new category info to the database

--- routes/categories.js
+++ routes/categories.js
@@ -3,6 +3,7 @@ const router = Router();
 const categoriesController = require('../controllers/categoriesController');
 
 router.get('/form/create', categoriesController.getCategoryForm);
+router.post("/create", categoriesController.createCategory)
 router.get('/', categoriesController.getCategories);
 
 module.exports = router;
