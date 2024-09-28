19d4b5d7a13971375d26307ae070498ee28acbb5
mayorr-star <immaboat11@gmail.com>
2024-09-28 13:13:29 +0000

    Add createCategory controller
    User will be able to create new categories

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -2,12 +2,12 @@ const db = require("../db/queries");
 const asyncHandler = require("express-async-handler");
 const getCurrentYear = require("../utilis/year");
 const NotFoundError = require("../utilis/errorhandling/errors/NotFoundError");
+const { body, validationResult } = require("express-validator");
 
 const currentYear = getCurrentYear();
 
 const getCategories = asyncHandler(async (req, res) => {
   const categories = await db.getCategories();
-  console.log(categories)
   if (!categories) {
     throw new NotFoundError("Not Found!");
   }
@@ -19,6 +19,23 @@ const getCategories = asyncHandler(async (req, res) => {
 
 const getCategoryForm = (req, res) => {
   res.render("categoryForm");
-}
+};
 
-module.exports = { getCategories, getCategoryForm };
+const validateCategory = [
+  body("category")
+    .trim()
+    .notEmpty()
+    .withMessage("Category is required")
+    .isAlpha()
+    .withMessage("Category must contain only alphabets")
+    .isLength({ min: 2 })
+    .withMessage("Category must be at least 2 charatcers long"),
+];
+
+const createCategory = [validateCategory, asyncHandler(async (req, res) => {
+  const { category } = req.body;
+  const errors = validationResult(req);
+  await db.insertCategory(category);
+})];
+
+module.exports = { getCategories, getCategoryForm, createCategory };
