50c3464333b2d131654b57fa0787b6ffa57b82e2
mayorr-star <immaboat11@gmail.com>
2024-09-28 11:40:56 +0000

    Add a post route to push updated changes to database

--- routes/produceRouter.js
+++ routes/produceRouter.js
@@ -2,10 +2,11 @@ const { Router } = require("express");
 const produceController = require("../controllers/produceController");
 const router = Router();
 
-router.get("/add-new-produce", produceController.getNewItemForm);
+router.get("/form/add-new-produce", produceController.getNewItemForm);
 router.post("/add-new-produce", produceController.createNewProduce);
+router.post("/update/:produceId", produceController.updateProduce);
+router.get("/form/update/:produceId", produceController.getUpdateForm);
 router.post("/delete/:produceId", produceController.deleteProduce);
-router.get("/update/:produceId", produceController.updateProduce);
 router.get("/:produceId", produceController.getProduceById);
 
 module.exports = router;
