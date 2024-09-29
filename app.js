d2e496f0ba5f6679cbe54e6f9d2e9c8cc16b7ebe
mayorr-star <immaboat11@gmail.com>
2024-09-29 16:18:19 +0000

    No changes made

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ const PORT = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/produce", produceRouter);
 app.use("/categories", categoryRouter);
