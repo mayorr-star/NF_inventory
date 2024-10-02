b6521ca3baa5db159622d488cc21af3e8e6f5d5e
mayorr-star <immaboat11@gmail.com>
2024-10-02 12:51:24 +0000

    Change database url based on environment whether production or development

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,15 @@
 const { Pool } = require("pg");
 
+let connectionString = null;
+
+switch (process.env.NODE_ENV) {
+  case "dev":
+    connectionString = process.env.DATABASE_LOCAL_URL;
+    break;
+  default:
+    connectionString = process.env.DATABASE_PUBLIC_URL;
+}
+
 module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: connectionString,
 });
