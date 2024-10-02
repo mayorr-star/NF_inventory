ecdf47fb0169979f232a19c8982ed43a4b7f21de
mayorr-star <immaboat11@gmail.com>
2024-10-02 15:31:17 +0000

    Modify

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-undef */
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  host: process.env.PGHOST || process.env.LOCAL_HOST,
+  user: process.env.PGUSER || process.env.LOCAL_USER,
+  database: process.env.PGDATABASE || process.env.LOCAL_DATABASE,
+  password: process.env.PGPASSWORD || process.env.LOCAL_PASSWORD,
+  port: parseInt(process.env.PGPORT || process.env.LOCALDB_PORT),
 });
