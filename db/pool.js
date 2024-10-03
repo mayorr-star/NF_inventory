39e912444670ea6892ad7a8bf023889e6971e75c
mayorr-star <immaboat11@gmail.com>
2024-10-03 11:14:23 +0000

    Setup connection for production and development

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,13 @@
 const { Pool } = require("pg");
 
+let connection = null;
+
+if (process.env.NODE_ENV === "development") {
+  connection = process.env.LOCAL_DATABASE_URL;
+} else {
+  connection = process.env.DATABASE_URL;
+}
+
 module.exports = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: connection,
 });
