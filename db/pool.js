626a0a82d974d51ee6d3838d4a29afe2bdd43d0c
mayorr-star <immaboat11@gmail.com>
2024-10-03 11:19:30 +0000

    Update

--- db/pool.js
+++ db/pool.js
@@ -3,9 +3,9 @@ const { Pool } = require("pg");
 let connection = null;
 
 if (process.env.NODE_ENV === "development") {
-  connection = process.env.LOCAL_DATABASE_URL;
+  connection = process.env.DB_URL;
 } else {
-  connection = process.env.DATABASE_URL;
+  connection = process.env.DATABASE_PUBLIC_URL;
 }
 
 module.exports = new Pool({
