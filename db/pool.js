b9d43f6c041d094d3579f954b939203ef719cf17
mayorr-star <immaboat11@gmail.com>
2024-10-03 10:56:39 +0000

    Use a connection URI to connect to database

--- db/pool.js
+++ db/pool.js
@@ -1,25 +1,5 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
-// pools will use environment variables
-// for connection information
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+module.exports = new Pool({
+  connectionString: process.env.DATABASE_URL
 });
-
-const checkDatabaseConnection = async () => {
-  try {
-    await pool.query("SELECT NOW()");
-    console.log("Database connected successfully!");
-  } catch (err) {
-    console.error("Could not connect to the database:", err);
-    process.exit(1); // Exit the application with an error code
-  }
-};
-
-checkDatabaseConnection();
-
-module.exports = pool;
